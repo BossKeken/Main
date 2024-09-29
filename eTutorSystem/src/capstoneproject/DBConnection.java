85854205815c4ad390f63bf4d9472ff47e9e4293
Keken <143066442+BossKeken@users.noreply.github.com>
2024-09-29 19:50:02 +0800

    Add files via upload

---
+++ eTutorSystem/src/capstoneproject/DBConnection.java
@@ -0,0 +1,29 @@
+package capstoneproject;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnection {
+    public Connection c;
+    public Statement s;
+
+    public DBConnection() {
+        try {
+            // Register JDBC Driver with Class's Static method
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///ETutorSystem", "root", "");
+            s = c.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println(e);
+        }
+    }
+
+    public void Close() {
+        try {
+            c.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
